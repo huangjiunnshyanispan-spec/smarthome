7c70df6695385d7e452d87c1e73e107e22d87e36
huangjiunnshyanispan-spec <huangjiunnshyanispan@gmail.com>
2026-04-28 14:39:26 +0800

    Add files via upload

---
+++ server/config/deviceConfig.js
@@ -0,0 +1,22 @@
+const DEVICE_VIEWS = {
+  smartDesk: 'smartDesk',
+  petFeeder: 'petFeeder',
+  lighting: 'lighting',
+  surveillance: 'surveillance',
+  smartGarage: 'smartGarage',
+  microphone: 'microphone'
+};
+
+const INITIAL_DEVICE_SIGNALS = {
+  smartDesk: '離線',
+  petFeeder: '離線',
+  lighting: '離線',
+  surveillance: '離線',
+  smartGarage: '離線',
+  microphone: '離線'
+};
+
+module.exports = {
+  DEVICE_VIEWS,
+  INITIAL_DEVICE_SIGNALS
+};
